7184b9f25e6cbd2c405691c82ffc02a7bc9f5c19
Matthew Tripodi <mtripodi1027@gmail.com>
2022-07-16 14:40:41 -0400

    Initial Commit

---
+++ ScrumWorks/ScrumWorksApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumWorksApp.swift
+//  ScrumWorks
+//
+//  Created by Matthew Tripodi on 7/16/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumWorksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
