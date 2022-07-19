2c46c0369d007456080916fcd96b333d2219c68a
Matthew Tripodi <mtripodi1027@gmail.com>
2022-07-18 20:29:50 -0400

    Make DailyScrum identifiable, make ScrumsView root ViewController

---
+++ ScrumWorks/ScrumsView.swift
@@ -0,0 +1,28 @@
+//
+//  ScrumsView.swift
+//  ScrumWorks
+//
+//  Created by Matthew Tripodi on 7/18/22.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    
+    var body: some View {
+        List {
+            ForEach(scrums) { scrum in
+                CardView(scrum: scrum)
+                    .listRowBackground(scrum.theme.mainColor)
+            }
+        }
+    }
+}
+
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrumsView(scrums: DailyScrum.sampleData)
+            .previewDevice(PreviewDevice(rawValue: "iPhone 12"))
+    }
+}
